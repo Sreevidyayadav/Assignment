1ee3127793c131d9389fa28691de6bfa17daa2b9
Sreevidyayadav <134796829+Sreevidyayadav@users.noreply.github.com>
2023-06-04 11:08:58 +0530

    Add files via upload

---
+++ JERRYCHASE.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    if(x<y){
+		        System.out.println("yes");
+		    }
+		    else{
+		        System.out.println("no");
+		    }
+		}
+	}
+}
