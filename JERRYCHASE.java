0678e775e5e5c5823e95b23658a67cf375c894ba
Sreevidyayadav <134796829+Sreevidyayadav@users.noreply.github.com>
2023-06-04 11:10:22 +0530

    Update JERRYCHASE.java
    Problem
    
    In a classic chase, Tom is running after Jerry as Jerry has eaten Tom's favourite food.
    
    Jerry is running at a speed of XX metres per second while Tom is chasing him at a speed of YY metres per second. Determine whether Tom will be able to catch Jerry.
    
    Note that initially Jerry is not at the same position as Tom.
    Input Format
    
        The first line of input will contain a single integer TT, denoting the number of test cases.
        Each test case consists of two space-separated integers XX and YY — the speeds of Jerry and Tom respectively.
    
    Output Format
    
    For each test case, output on a new line, YES, if Tom will be able to catch Jerry. Otherwise, output NO.
    
    You can print each character in uppercase or lowercase. For example NO, no, No, and nO are all considered the same.
    Constraints
    
        1≤T≤1001≤T≤100
        1≤X,Y≤101≤X,Y≤10
    
    Sample 1:
    Input
    Output
    
    4
    2 3
    4 1
    1 1
    3 5
    
    YES
    NO
    NO
    YES
    
    Explanation:
    
    Test case 11: Jerry is running at the speed of 22 metres per second while Tom is chasing him at the speed of 33 metres per second. Since Jerry's speed is less than Tom's, Tom will eventually catch Jerry.
    
    Test case 22: Jerry is running at the speed of 44 metres per second while Tom is chasing him at the speed of 11 metres per second. Since Jerry's speed is higher than Tom's, Tom will never be able to catch Jerry.
    
    Test case 33: Jerry is running at the speed of 11 metre per second while Tom is chasing him at the speed of 11 metre per second. Since Jerry's speed is same as that of Tom's and both of them are not at the same position, Tom will never be able to catch Jerry.
    
    Test case 44: Jerry is running at the speed of 33 metres per second while Tom is chasing him at the speed of 55 metres per second. Since Jerry's speed is less than Tom's, Tom will eventually catch Jerry.

--- JERRYCHASE.java
+++ JERRYCHASE.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class GFG {
+class JerryChase {
 	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
@@ -10,10 +10,10 @@ class GFG {
 		    int x = sc.nextInt();
 		    int y = sc.nextInt();
 		    if(x<y){
-		        System.out.println("yes");
+		        System.out.println("yes, Tom will be able to catch Jerry.");
 		    }
 		    else{
-		        System.out.println("no");
+		        System.out.println("no,Tom will not be able to catch Jerry.");
 		    }
 		}
 	}
